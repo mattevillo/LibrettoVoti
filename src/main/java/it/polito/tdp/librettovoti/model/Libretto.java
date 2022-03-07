27722e60bfb4f341899989b816c9cf968c7c6070
Matteo Villosio <s258803@studenti.polito.it>
2022-03-07 11:07:24 +0100

    Versione 1.0

---
+++ src/main/java/it/polito/tdp/librettovoti/model/Libretto.java
@@ -0,0 +1,29 @@
+package it.polito.tdp.librettovoti.model;
+import java.util.*;
+
+public class Libretto {
+	
+	private List<Voto> voti;
+	
+	public Libretto() {
+		this.voti = new ArrayList<>();
+	}
+	
+
+	public void add(Voto voto1) {
+		voti.add(voto1);
+		
+	}
+
+
+	public String toString() {
+		String s ="";
+		for(Voto v: this.voti) {
+			s= s+ v.toString()+"\n";
+		}
+		return s;
+	}
+	
+	
+
+}
