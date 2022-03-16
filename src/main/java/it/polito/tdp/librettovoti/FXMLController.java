b8b9dba5e7ca3e05d1ece1dfa1eda94ae81adb16
Matteo Villosio <s258803@studenti.polito.it>
2022-03-16 16:01:47 +0100

    Versione 2.0 - Add Interfaccia

--- src/main/java/it/polito/tdp/librettovoti/FXMLController.java
+++ src/main/java/it/polito/tdp/librettovoti/FXMLController.java
@@ -1,25 +1,78 @@
 package it.polito.tdp.librettovoti;
 
 import java.net.URL;
+import java.time.LocalDate;
 import java.util.ResourceBundle;
+
+import it.polito.tdp.librettovoti.model.Libretto;
+import it.polito.tdp.librettovoti.model.Voto;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 
-public class FXMLController implements Initializable {
+public class FXMLController {
+	
+	private Libretto model;
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private ComboBox<Integer> cmbPunti;
+
+    @FXML
+    private TextField txtNome;
+
+    @FXML
+    private TextArea txtVoti;
+
+    @FXML
+    private Label txtStatus;
     
     @FXML
-    private Label label;
-    
-    @FXML
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
+    void handleNuovoVoto(ActionEvent event) {
+    	
+
+    	String nome = txtNome.getText();
+    	Integer punti = cmbPunti.getValue();
+    	
+    	if(nome.equals("") || punti==null) {
+    		//ERRORE
+    		txtStatus.setText("ERRORE: occore inserire nome e voto\n");
+    		return;
+    	}
+    		
+    		
+    	
+    	model.add(new Voto(nome, punti, LocalDate.now()));
+    	
+    	String contenutoLibretto = model.toString();
+    	txtVoti.setText(contenutoLibretto);
+    	
+    	txtNome.clear();
+    	cmbPunti.setValue(null);
+    	txtStatus.setText("");
     }
     
-    @Override
-    public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
+    public void setModel(Libretto model) {
+    	this.model=model;
+    }
+
+    @FXML
+    void initialize() {
+        assert cmbPunti != null : "fx:id=\"cmbPunti\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert txtNome != null : "fx:id=\"txtNome\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert txtVoti != null : "fx:id=\"txtVoti\" was not injected: check your FXML file 'Scene.fxml'.";
+
+        cmbPunti.getItems().clear();
+        for (int p=18; p<=30; p++)
+        	cmbPunti.getItems().add(p);     
+    }
+
 }
